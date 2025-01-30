257b6db22645499656d7145bc6ef182da2029b6e
shrey <shrey.shreyashh@gmail.com>
2025-01-30 18:40:58 +0530

    qr generation based on url

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const class1 = document.querySelector('.class1');
 const class2 = document.querySelector('.class2');
+const urlContainer = document.querySelector('.urlContainer')
 
 
 class1.addEventListener('click', ()=>{
     class2.classList.remove('selected')
     class1.classList.toggle('selected')
 
+    urlContainer.classList.remove('hidden');
 
 
 
-
-
+    
 })
 
 
@@ -19,9 +20,33 @@ class2.addEventListener('click', ()=>{
     class2.classList.toggle('selected')
 
 
+    urlContainer.classList.toggle('hidden')
 
-
-
-
-    
 })
+
+let qrcode = new QRCode(
+    document.querySelector('.qrcode')
+);
+
+qrcode.makeCode("nothing");
+
+
+
+
+function generateQR(){
+    if (
+        document.querySelector("input")
+            .value === "" ||
+        document.querySelector("input")
+            .value === " ") {
+        alert(
+            "Input Field Can not be blank!"
+        );
+    } 
+    else {
+        qrcode.makeCode(
+            document.querySelector(
+                "input"
+            ).value);
+    }
+}
