b3c66247cff21653aa02ebeefbe97371ea428e43
shrey <shrey.shreyashh@gmail.com>
2025-01-30 19:42:48 +0530

    implemented the download function

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ class1.addEventListener('click', ()=>{
 
     urlContainer.classList.remove('hidden');
     createBtn.classList.remove('hidden');
+
+    
 })
 
 
@@ -52,4 +54,28 @@ function generateQR(){
         downlaodBtn.classList.remove('hidden');
         createBtn.classList.toggle('hidden');
     }
+}
+
+
+function download(){
+    
+    const img = document.querySelector('.qrcode img');
+
+    fetch(img.src)
+        .then(response => response.blob())
+        .then(blob => {
+            const url = URL.createObjectURL(blob);
+            const a = document.createElement("a");
+            a.href = url;
+            a.download = "qrcode.png"; 
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+            URL.revokeObjectURL(url);
+        })
+        .catch(error => console.error("Error downloading image:", error));
+
+
+
+    location.reload();
 }
